8299e728e564829749c5800b20c374845c955a4e
Rubin Khadka <rubinkhadka84@gmail.com>
2026-07-18 21:06:33 +0200

    additional files

---
+++ debug_gpio/Debug/Src/gpio.d
@@ -0,0 +1,15 @@
+Src/gpio.o: ../Src/gpio.c \
+ /home/rubin/STM32Cube/Repository/CMSIS-Device/stm32f103xb.h \
+ /home/rubin/STM32Cube/Repository/CMSIS-Core/core_cm3.h \
+ /home/rubin/STM32Cube/Repository/CMSIS-Core/cmsis_version.h \
+ /home/rubin/STM32Cube/Repository/CMSIS-Core/cmsis_compiler.h \
+ /home/rubin/STM32Cube/Repository/CMSIS-Core/cmsis_gcc.h \
+ /home/rubin/STM32Cube/Repository/CMSIS-Device/system_stm32f1xx.h \
+ ../Inc/gpio.h
+/home/rubin/STM32Cube/Repository/CMSIS-Device/stm32f103xb.h:
+/home/rubin/STM32Cube/Repository/CMSIS-Core/core_cm3.h:
+/home/rubin/STM32Cube/Repository/CMSIS-Core/cmsis_version.h:
+/home/rubin/STM32Cube/Repository/CMSIS-Core/cmsis_compiler.h:
+/home/rubin/STM32Cube/Repository/CMSIS-Core/cmsis_gcc.h:
+/home/rubin/STM32Cube/Repository/CMSIS-Device/system_stm32f1xx.h:
+../Inc/gpio.h:
